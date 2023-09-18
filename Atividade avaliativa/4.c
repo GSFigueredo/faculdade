f38ecc576b40a5cbb60c90a04c9a4a8fc8805d64
Gabriel Figueredo <gabriel.sfigueredo8@gmail.com>
2023-09-18 23:41:51 +0000

    7101665

--- Atividade avaliativa/4.c
+++ Atividade avaliativa/4.c
@@ -14,11 +14,23 @@ bool validarDados(float a, float b, float c) {
 int main () {
 
     float a, b, c;
+    int tipo;
 
     printf("Digite o valor dos três lados de um triângulo:");
     scanf("%f %f %f", &a, &b, &c);
 
     if(validarDados(a, b, c) == true) {
+        if(a == b && b == c) {
+            printf("Triangulo equilatero");
+            tipo = 1;
+        } else if (a != b && a != c && b != c) {
+            printf("Triangulo escaleno");
+            tipo = 2;
+        } else {
+            printf("Triangulo isóceles");
+            tipo = 3;
+        }
+
         
     }
 
