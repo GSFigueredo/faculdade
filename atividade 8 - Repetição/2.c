abcbe5e4ce2706f542fcf28e53eb2d5443859461
Gabriel Figueredo <gabriel.sfigueredo8@gmail.com>
2023-10-02 13:36:45 +0000

    701665

--- atividade 8 - Repetição/2.c
+++ atividade 8 - Repetição/2.c
@@ -5,9 +5,10 @@
 int main () {
 
     int tot = 0;
-    int elevA = 0, elevB = 0, elevC = 0, mat, vesp, not;
+    int elevMedioV = 0;
+    int elevA = 0, elevB = 0, elevC = 0, mat = 0, vesp = 0, not = 0;
     int maior = 0, menor = 10, maiorP = 0, menorP = 10;
-    char elev, periodo, elevMais, elevMedio, elevMenos, periodoMais, periodoMenos;
+    char elev, periodo, elevMais, elevMenos, periodoMais, periodoMenos;
 
     for(int c = 0; c < 6; c++) {
 
@@ -18,110 +19,100 @@ int main () {
 
         getchar();
 
-        if (strcmp(elev, "A") == 0 || strcmp(elev, "a") == 0) {
+        if (elev == 'A' || elev == 'a') {
             elevA++;
 
             if(elevA > maior) {
                 maior = elevA;
-                strcpy(elevMais, "A");
+                elevMais = 'A';
             }
 
             if(elevA < menor) {
                 menor = elevA;
-                strcpy(elevMenos, "A");
+                elevMenos = 'A';
             }
         }
 
-        if (strcmp(elev, "B") == 0 || strcmp(elev, "b") == 0) {
+        if (elev == 'B' || elev == 'b') {
             elevB++;
 
             if(elevB > maior) {
                 maior = elevB;
-                strcpy(elevMais, "B");
+                 elevMais = 'B';
             }
 
             if(elevB < menor) {
                 menor = elevB;
-                strcpy(elevMenos, "B");
+                elevMenos = 'B';
             }
         }
 
-        if (strcmp(elev, "C") == 0 || strcmp(elev, "c") == 0) {
+        if ((elev == 'C' || elev == 'c')) {
             elevC++;
 
             if(elevC > maior) {
                 maior = elevC;
-                strcpy(elevMais, "C");
+                 elevMais = 'C';
             }
 
             if(elevC < menor) {
                 menor = elevC;
-                strcpy(elevMenos, "V");
+                elevMenos = 'V';
             }
         }
 
         printf("Qual o período que você mais utiliza? [M/V/N]");
         scanf("%c", &periodo);
 
-        if(strcmp(periodo, "M") == 0 || strcmp(periodo, "m") == 0) {
+        getchar();
+
+        if(periodo == 'M' || periodo == 'm') {
 
             mat++;
 
              if(mat > maiorP) {
                 maiorP = mat;
-                strcpy(periodoMais, "M");
+                periodoMais = 'M';
             }
 
             if(mat < menorP) {
                 menorP = mat;
-                strcpy(periodoMenos, "M");
+                periodoMenos = 'M';
             }
         }
 
-        if(strcmp(periodo, "V") == 0 || strcmp(periodo, "v") == 0) {
+        if(periodo == 'V' || periodo == 'v') {
 
             vesp++;
 
              if(vesp > maiorP) {
                 maiorP = vesp;
-                strcpy(periodoMais, "V");
+                periodoMais = 'V';
             }
 
             if(vesp < menorP) {
                 menorP = vesp;
-                strcpy(periodoMenos, "V");
+                periodoMenos = 'V';
             }
         }
 
-        if(strcmp(periodo, "N") == 0 || strcmp(periodo, "n") == 0) {
+        if(periodo == 'N' || periodo == 'n'){
 
             not++;
 
              if(not > maiorP) {
                 maiorP = not;
-                strcpy(periodoMais, "N");
+                periodoMais = 'N';
             }
 
             if(not < menorP) {
                 menorP = not;
-                strcpy(periodoMenos, "N");
+                periodoMenos = 'N';
             }
         }
 
-    }
-
-    if(strcmp(elevMais, "A") && strcmp(elevMenos, "B")) {
-        strcpy(elevMedio, "C");
-    } else if(strcmp(elevMais, "B") && strcmp(elevMenos, "A")) {
-        strcpy(elevMedio, "C");
-    } else if (strcmp(elevMais, "A") && strcmp(elevMenos, "C")) {
-        strcpy(elevMedio, "B");
-    } else if (strcmp(elevMais, "C") && strcmp(elevMenos, "A")) {
-        strcpy(elevMedio, "B");
-    } else if (strcmp(elevMais, "B") && (elevMenos, "C")) {
-        strcpy(elevMedio, "A");
-    } else if (strcmp(elevMais, "C") && (elevMenos, "B")) {
-        strcpy(elevMedio, "A");
+        elevMedioV = maior - menor;
+        
     }
 
     printf("O período mais utilizado: %c e o elevador que pertence: %c", periodoMais, elevMais);
@@ -130,7 +121,7 @@ int main () {
     printf("\n");
 
     float porcento = (maiorP*100)/tot;
-    float porcentoMedio = (elevMedio*100)/tot;
+    float porcentoMedio = (elevMedioV*100)/tot;
 
     printf("Diferença percentual entre o horário mais usado e o menos usado: %.2f", porcento);
     printf("\n");
