f11e9945988da6f73346be9c27e38964362bdcd2
Gabriel Figueredo <gabriel.sfigueredo8@gmail.com>
2023-10-03 23:46:13 +0000

    701665

--- atividade 9 - vetores/9.c
+++ atividade 9 - vetores/9.c
@@ -2,19 +2,27 @@
 #include <math.h>
 #include <string.h>
 
-int main () {
+int main(){
 
     int vet[6];
 
     printf("Digite seis valores pares para o vetor:");
-    for(int c = 0; c<6; c++) {
+    for (int c = 0; c < 6; c++){
+
         scanf("%d", &vet[c]);
+
+        if (vet[c] % 2 != 0){
+            printf("Valor inválido");
+            return 0;
+        }
+
     }
+
 
     printf("Valores na ordem inversa:");
     printf("\n");
 
-    for(int c = 5; c>-1; c--) {
+    for (int c = 5; c > -1; c--){
         printf("%d", vet[c]);
         printf("\n");
     }
