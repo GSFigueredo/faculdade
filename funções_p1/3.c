13cb8933c93d85112ff99a7670e8d384a6bffdd1
Gabriel Figueredo <gabriel.sfigueredo8@gmail.com>
2023-10-30 22:41:17 +0000

    701665

---
+++ funções_p1/3.c
@@ -0,0 +1,39 @@
+#include <stdio.h> 
+#include <math.h>
+#include <string.h> 
+
+int main() {
+
+   /* int mat[10][10];
+
+    for(int l = 1; l < 11; l++) {
+
+        for(int c = 1; c < 11; c++) {
+            printf("Digite o valor da linha %d coluna %d: ", l, c);
+            scanf("%d", &mat[l][c]);
+        }
+
+    }
+*/
+
+    int mat[2][2];
+
+    for(int l = 1; l < 3; l++) {
+
+        for(int c = 1; c < 3; c++) {
+            printf("Digite o valor da linha %d coluna %d: ", l, c);
+            scanf("%d", &mat[l][c]);
+        }
+
+    }
+
+    for(int l = 1; l < 3; l++) {
+        printf("\n");
+        for(int c = 1; c < 3; c++) {
+            printf("%d", mat[l][c]);
+        }
+
+    }
+
+    return 0;
+}
