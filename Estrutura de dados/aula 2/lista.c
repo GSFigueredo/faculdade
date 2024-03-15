bd42a2350668d59898cbd442fe16f75374cc1707
Gabriel Figueredo <gabriel.sfigueredo8@gmail.com>
2024-03-15 00:15:44 +0000

    commit

--- Estrutura de dados/aula 2/lista.c
+++ Estrutura de dados/aula 2/lista.c
@@ -1,26 +1,72 @@
-#include <stdio.h> 
-#include <math.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "lista.h"
 
-typedef struct aluno{
-   char nome [50];
-   int idade;
-   char curso [50];
-   float nota;
+// Variáveis
+
+    int cont = 0;
+
+// Fim variáveis
+
+
+struct Aluno{
+    char nome[50];
+    int idade;
+    char curso[50];
+    float nota;
 } aluno;
 
+struct Aluno *criarLista(int numAlunos){
 
-void criarLista(int numAlunos) {
-    
+    struct Aluno *alunos = (struct Aluno*)malloc(numAlunos * sizeof(aluno));
+
+    return alunos;
 }
 
-void registrarAluno () { 
-    printf("Digite o nome do aluno:");
-    fgets("")
+void adicionarAlunos(struct Aluno **lista, struct Aluno aluno) {
+
+    strcpy(lista[cont]->nome, aluno.nome);
+    lista[cont]->idade = aluno.idade;
+    lista[cont]->nota = aluno.nota;
+
+    cont++;
+
 }
 
-int main () {
+void listarAlunos (Aluno *lista) {
+    for(int i = 0; i < cont; i++) {
+        printf("Aluno: %s", lista[i].nome);
+        printf("\nIdade: %d", lista[i].idade);
+        printf("\nCurso: %s", lista[i].curso);
+        printf("\nNota: %.2f", lista[i].nota);
+    }
+}
 
-    registrarAluno();
+int main(){
+
+    struct Aluno *alunos;
+
+    alunos =  criarLista(10);
+
+    struct Aluno aluno1, aluno2;
+
+    strcpy(aluno1.nome, "Gabriel");
+    aluno1.idade = 18;
+    strcpy(aluno1.curso, "CCC");
+    aluno1.nota = 10;
+
+    adicionarAlunos(&alunos, aluno1);
+
+    strcpy(aluno2.nome, "Joao");
+    aluno2.idade = 19;
+    strcpy(aluno2.curso, "CCC");
+    aluno2.nota = 8;
+
+    adicionarAlunos(&alunos, aluno2);
+
+    listarAlunos(alunos);
 
     return 0;
 }
--- Estrutura de dados/aula 2/lista.h
+++ Estrutura de dados/aula 2/lista.h
@@ -1,2 +1,12 @@
-#indef alunos_h
-#indef a
+#ifndef ALUNOS_H
+#define ALUNOS_H
+
+typedef struct Aluno Aluno;
+
+struct Aluno criaLista(int numAlunos);
+void listarAlunos(Aluno *lista);
+void adicionarAluno(Aluno **lista, Aluno aluno);
+Aluno *buscarAluno(Aluno *lista, int matricula);
+void removerAluno(Aluno **lista, int matricula);
+
+#endif
