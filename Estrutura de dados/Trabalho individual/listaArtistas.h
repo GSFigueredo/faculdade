3e8562b30bfa07abf85fc4ae79afff53501ef2da
Gabriel Figueredo <gabriel.sfigueredo8@gmail.com>
2024-04-27 19:14:19 +0000

    Projeto finalizado!

--- Estrutura de dados/Trabalho individual/listaArtistas.h
+++ Estrutura de dados/Trabalho individual/listaArtistas.h
@@ -1,11 +1,6 @@
 #ifndef LISTA_ARTISTAS_H
 #define LISTA_ARTISTAS_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-
 typedef struct artistas{
     char nome[50];
     char tipoMusc[50];
@@ -16,14 +11,15 @@ typedef struct artistas{
 
 void limparCaractere ();
 void iniciarLista(artistas listaArtistas[], int *posicaoArtista);
-void inserirArtista (artistas listaArtistas[], int *posicaoArtista);
 int ordenarAlfabeticamente(const void *a, const void *b);
 void limparString(char *str);
 void reescreverLista(artistas listaArtistas[], int quantidadeArtistas);
+void inserirArtista (artistas listaArtistas[], int *posicaoArtista);
 void removerArtista(artistas listaArtistas[], int *posicaoArtista, char nomeArtista[100]);
-void editarArtista(artistas listaArtistas[], int c, char tipoEdicao[100]);
+void editarArtista(artistas listaArtistas[], int c, char tipoEdicao[100], int posicaoArtista);
 void tipoEdicao(artistas listaArtistas[], int *posicaoArtista, char nomeArtista[200]);
 void buscarAlbum(artistas listaArtistas[], int quanArtistas, char nomeAlbum[200]);
 void buscarBinario(artistas listaArtistas[], int inicio, int fim, char nomeArtista[]);
+
 
 #endif
